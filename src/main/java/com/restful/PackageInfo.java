5b424effbe1dbdfbf1b955c5c0d533ef28cdbfcd
Elone Hoo <huchengyea@163.com>
2022-02-27 11:12:30 +0800

    Delete src directory

--- src/main/java/com/restful/PackageInfo.java
+++
@@ -1,6 +0,0 @@
-/**
- * This will store the source code.
- * version v0.0.1
- */
-
-package com.restful;
--- src/test/java/com/restful/PackageInfo.java
+++
@@ -1,6 +0,0 @@
-/**
- * Here the test code will be run.
- * version v0.0.1
- */
-package com.restful;
-
