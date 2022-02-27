bbaecfed8809245679cbc644801ae435239c542f
xiaoxunyao <huchengyea@163.com>
2022-02-27 10:21:39 +0800

    release v0.0.1

---
+++ src/main/java/com/restful/PackageInfo.java
@@ -0,0 +1,6 @@
+/**
+ * This will store the source code.
+ * version v0.0.1
+ */
+
+package com.restful;
---
+++ src/test/java/com/restful/PackageInfo.java
@@ -0,0 +1,6 @@
+/**
+ * Here the test code will be run.
+ * version v0.0.1
+ */
+package com.restful;
+
