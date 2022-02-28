35b743664be39a6fbeea0b938921c7a84ccbe1b3
xiaoxunyao <huchengyea@163.com>
2022-02-28 18:25:36 +0800

    test: test builder mode

--- src/test/java/com/restful/BuildTest.java
+++ src/test/java/com/restful/BuildTest.java
@@ -1,13 +1,8 @@
 package com.restful;
 
-
-import com.restful.header.HttpHeaders;
-import com.restful.http.HttpStatus;
-
 public class BuildTest {
 	public static void main(String[] args) {
-		Result.status(HttpStatus.BAD_GATEWAY).headers(HttpHeaders.EMPTY).body("1");
-		Result.status(200).headers(HttpHeaders.EMPTY).body("1");
+
 	}
 
 }
